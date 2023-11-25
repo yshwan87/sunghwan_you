064ac86e9fa58a5bad28505b6d0d653b8d033fa3
yshwan87 <77893199+yshwan87@users.noreply.github.com>
2023-11-25 14:00:30 -0800

    Confirm delete page

--- contactManagement/urls.py
+++ contactManagement/urls.py
@@ -14,5 +14,5 @@ urlpatterns = [
     path("create/", ContactCreateView.as_view(), name="contact_create"),
     path("contacts/<int:pk>/", ContactDetailView.as_view(), name="contact_detail"),
     path("contacts/<int:pk>/update/", ContactUpdateView.as_view(), name="contact_update"),
-    path("contacts/<int:pk>/delete/", ContactDeleteView.as_view(), name="contact_delete"),
+    path("contacts/<int:pk>/delete/", ContactDeleteView.as_view(), name="contact_confirm_delete"),
 ]
